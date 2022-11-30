5de0c81af953f7c5265a9d2ba2d3262e2fb30d3e
b-sandrine <sanrdushimimana@gmail.com>
2022-11-30 08:07:18 +0200

    Finish login & created events service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  loginUserData: any = {}
+  constructor(private _auth: AuthService, private _router: Router) {}
+  ngOnInit():void {}
+  loginUser() {
+    this._auth.loginUser(this.loginUserData)
+    .subscribe(
+      res => {
+        console.log(res)
+        localStorage.setItem("token", res.token)
+        this._router.navigate(['/special'])
+      }
+    , err => console.log(err))
+  }
 }
